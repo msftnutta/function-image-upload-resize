d0bc29ffc28003dd7f2dcd31b0c1b77eeccb29df
Nuttapong Wong-aree <98095529+msftnutta@users.noreply.github.com>
2023-10-16 18:47:11 +0800

    Update Thumbnail.cs

--- ImageFunctions/Thumbnail.cs
+++ ImageFunctions/Thumbnail.cs
@@ -93,7 +93,8 @@ namespace ImageFunctions
                         var blobName = GetBlobNameFromUrl(createdEvent.Url);
 
                         using (var output = new MemoryStream())
-                        using (Image<Rgba32> image = Image.Load(input))
+                        using (Image<Rgba32> image = Image.Load<Rgba32>(input)) 
+                        // Explicitly specify Rgba32 here
                         {
                             var divisor = image.Width / thumbnailWidth;
                             var height = Convert.ToInt32(Math.Round((decimal)(image.Height / divisor)));
